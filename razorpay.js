4f050821561db32788ac70f60b5708411521e754
SOWMIYAN-S <140789716+SOWMIYAN-S@users.noreply.github.com>
2024-02-16 14:48:37 +0530

    Add files via upload

--- razorpay.js
+++ razorpay.js
@@ -2,18 +2,18 @@
 function makePayment() {
   var phone = document.getElementById('phone').value;
   var email = document.getElementById('email').value;
-
-  // You can add more data like item name, amount, etc., to be sent to Razorpay
+  var amount = document.getElementById("customAmount").value * 100;
   var options = {
     key: 'rzp_live_nTBD5eI6WnX8hg',
-    amount: 10000, // Example amount in paise (100 paise = 1 INR)
+    amount: amount, // 100 paise = 1 INR
     currency: 'INR',
-    name: 'EMERALZ',
-    description: 'Purchase Description',
+    name: 'EMARALZ',
+    description: ' ',
     handler: function(response) {
       // Handle the success callback after payment completion
       alert('Payment successful! Payment ID: ' + response.razorpay_payment_id);
-      // You can redirect to a thank you page or handle the success as per your requirement
+
+    
     },
     prefill: {
       contact: phone,
